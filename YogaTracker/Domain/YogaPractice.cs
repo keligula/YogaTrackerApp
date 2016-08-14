4d52f203a7210ac02709e10039101ecf3d22d1cb
keligula <keldonger@gmail.com>
2016-08-13 17:35:58 -0700

    Update YogaPractice.cs

--- YogaTracker/Domain/YogaPractice.cs
+++ YogaTracker/Domain/YogaPractice.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -15,10 +15,6 @@ namespace KellyProject.Domain
         public DateTime PracticeDate { get; set; }
 
         public string InstructorName { get; set; }
-
-        //public string ClassType { get; set; }
-
-        //public int Rating { get; set; }
 
     }
 }
