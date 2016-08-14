c8377574e209c9f9157d078d749ded8702754221
keligula <keldonger@gmail.com>
2016-08-13 17:31:51 -0700

    Update practiceListController.js

--- YogaTracker/AngularJS/practiceListController.js
+++ YogaTracker/AngularJS/practiceListController.js
@@ -1,4 +1,4 @@
-﻿
+
 (function () {
     "use strict";
 
@@ -12,20 +12,16 @@
         var vm = this;
         vm.practices = null;
         
-        //vm.yogiOptions = [{ YogiId: 1, YogiName: "Kelly" }, { YogiId: 2, YogiName: "Anthony" }];
-
         vm.$scope = $scope;
         vm.$yogaService = $yogaService;
     
         $baseController.merge(vm, $baseController);
 
-
         render();
 
         function render() {
-            console.log("This is my list of yoga practices.")
+            console.log("This is the list of all yoga practices.")
             vm.$yogaService.getAllPractices(_onGetAllPracticesSuccess, _onGetAllPracticesError);
-
         };
 
         function _onGetAllPracticesSuccess(response) {
@@ -36,37 +32,6 @@
         function _onGetAllPracticesError(jqXhr, error) {
             vm.$alertService.error();
         };
-
-        //function _getPracticesByYogi(yogiId) {
-        //    console.log("Button is working");
-        //    vm.$yogaService.getPracticeByYogiId(yogiId, _onGetByYogiSuccess, _onGetByYogiError);
-
-        //};
-       
-        //function _onGetByYogiSuccess() {
-        //    vm.practices = data.items;
-        //    console.log("on get success", data)
-        //};
-
-        //function _onGetByYogiError(jqXhr, error) {
-        //    vm.$alertService.error();
-        //};
-
-        //function _getAllPractices() {
-        //    console.log("Button is working");
-        //    vm.$yogaService.getAllPractices(_onGetAllPracticesSuccess, _onGetAllPracticesError);
-
-        //};
-
-        //function _onGetAllPracticesSuccess(data) {
-        //    vm.practices = data.items;
-        //    console.log("on get success", data)
-        //};
-
-        //function _onGetAllPracticesError(jqXhr, error) {
-        //    vm.$alertService.error();
-        //};
-
 
     }
 })();
